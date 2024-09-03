7185904e1ea296178a2e06eda292a9f1690a9282
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-09-04 00:40:50 +0500

    update

--- client/src/components/PayContents/CheckoutForm/CheckoutForm.jsx
+++ client/src/components/PayContents/CheckoutForm/CheckoutForm.jsx
@@ -60,7 +60,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "https://fyp-testing.vercel.app/success",
+        return_url: "https://servicelance.vercel.app/success",
       },
     });
 
